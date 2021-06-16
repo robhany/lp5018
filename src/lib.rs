9d38be6f3cb61d48284b93f65ead013ce53f68da
Robert Hanyecz <rh@wertschuetz.de>
2021-06-16 14:09:01 +0200

    rename function to better function name

--- src/lib.rs
+++ src/lib.rs
@@ -4,13 +4,12 @@ extern crate embedded_hal as hal;
 use crate::Output::*;
 use hal::blocking::i2c::{Write, WriteRead};
 
+//settings in binary format, so they are comparable with the values from the datasheet
 const BROADCAST_ADDRESS: u8 = 0b11_1100;
 const BASE_ADDRESS: u8 = 0b10_1000;
 const ADDR0_PIN_HIGH: u8 = 0b0001;
 const ADDR1_PIN_HIGH: u8 = 0b0010;
-
 const CONFIG_0_CHIP_ENABLE: u8 = 0b0100_0000;
-
 // const CONFIG_1_LED_GLOBAL_OFF: u8 = 0b0000_0001;
 // const CONFIG_1_INCREASE_MAX_CURRENT_TO_35MA: u8 = 0b0000_0010;
 const CONFIG_1_PWM_DITHERING_ENABLED: u8 = 0b0000_0100;
@@ -130,27 +129,27 @@ impl LedDriver {
     where
         I2C: Write<Error = E> + WriteRead<Error = E>,
     {
-        self.set_output(i2c, Out00, intensity)?;
-        self.set_output(i2c, Out01, intensity)?;
-        self.set_output(i2c, Out02, intensity)?;
-        self.set_output(i2c, Out03, intensity)?;
-        self.set_output(i2c, Out04, intensity)?;
-        self.set_output(i2c, Out05, intensity)?;
-        self.set_output(i2c, Out06, intensity)?;
-        self.set_output(i2c, Out07, intensity)?;
-        self.set_output(i2c, Out08, intensity)?;
-        self.set_output(i2c, Out09, intensity)?;
-        self.set_output(i2c, Out10, intensity)?;
-        self.set_output(i2c, Out11, intensity)?;
-        self.set_output(i2c, Out12, intensity)?;
-        self.set_output(i2c, Out13, intensity)?;
-        self.set_output(i2c, Out14, intensity)?;
-        self.set_output(i2c, Out15, intensity)?;
-        self.set_output(i2c, Out16, intensity)?;
-        self.set_output(i2c, Out17, intensity)
+        self.change_intensity_for_output(i2c, Out00, intensity)?;
+        self.change_intensity_for_output(i2c, Out01, intensity)?;
+        self.change_intensity_for_output(i2c, Out02, intensity)?;
+        self.change_intensity_for_output(i2c, Out03, intensity)?;
+        self.change_intensity_for_output(i2c, Out04, intensity)?;
+        self.change_intensity_for_output(i2c, Out05, intensity)?;
+        self.change_intensity_for_output(i2c, Out06, intensity)?;
+        self.change_intensity_for_output(i2c, Out07, intensity)?;
+        self.change_intensity_for_output(i2c, Out08, intensity)?;
+        self.change_intensity_for_output(i2c, Out09, intensity)?;
+        self.change_intensity_for_output(i2c, Out10, intensity)?;
+        self.change_intensity_for_output(i2c, Out11, intensity)?;
+        self.change_intensity_for_output(i2c, Out12, intensity)?;
+        self.change_intensity_for_output(i2c, Out13, intensity)?;
+        self.change_intensity_for_output(i2c, Out14, intensity)?;
+        self.change_intensity_for_output(i2c, Out15, intensity)?;
+        self.change_intensity_for_output(i2c, Out16, intensity)?;
+        self.change_intensity_for_output(i2c, Out17, intensity)
     }
 
-    pub fn set_output<I2C, E>(
+    pub fn change_intensity_for_output<I2C, E>(
         &self,
         i2c: &mut I2C,
         output_pin: Output,
